9d89e3c7acb9aea8ff78ea2a09db062fd2de2da1
Patel Nishant <npatel682@rku.ac.in>
2025-01-21 11:35:57 +0530

    21-1

---
+++ login.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="DEMO1.login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="UserName" runat="server" Text="UserName"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="uname" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Password"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="Pass" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ login.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DEMO1
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string username = Pass.Text.Trim().ToString();
+            string password = uname.Text.Trim().ToString();
+
+            string constr = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\Nishant Patel\\source\\repos\\DEMO1\\App_Data\\Database2.mdf\";Integrated Security=True";
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+
+            String query = "select * from Users where UserEmail = '" + username + "' and Password = '" + password + "'";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                Session["UserEmail"] = username;
+                Response.Redirect("Home.aspx");
+                Response.Write("<script>alert('Login Successful')</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('Invalid Username or Password')</script>");
+            }
+        }
+    }
+}
